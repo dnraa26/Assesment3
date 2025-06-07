d92832d7c1a89f758667736772f082e7ee5ec0fe
Danira Ariani Ningtyas <ariani.ixk@gmail.com>
2025-06-07 15:30:49 +0700

    Menampilkan grid untuk gambar nantinya

--- app/src/main/java/com/danira0037/assesment3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/danira0037/assesment3/ui/screen/MainScreen.kt
@@ -1,7 +1,14 @@
 package com.danira0037.assesment3.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -9,12 +16,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.danira0037.assesment3.R
+import com.danira0037.assesment3.model.Buku
+import com.danira0037.assesment3.network.BukuApi
 import com.danira0037.assesment3.ui.theme.Assesment3Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -41,8 +56,28 @@ fun MainScreen() {
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
+    val data by viewModel.data
 
-    Text("Hello Android", modifier = modifier)
+    LazyVerticalGrid(
+        modifier = modifier.fillMaxSize().padding(4.dp),
+        columns = GridCells.Fixed(2),
+    ) {
+        items(data) { ListItem(buku = it) }
+    }
+}
+
+@Composable
+fun ListItem(buku: Buku) {
+    Box(
+        modifier = Modifier.padding(4.dp).border(1.dp, Color.Gray)
+    ) {
+        AsyncImage(
+            model = buku.gambar,
+            contentDescription = stringResource(R.string.gambar, buku.namaBuku),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth().padding(4.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/danira0037/assesment3/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/danira0037/assesment3/ui/screen/MainViewModel.kt
@@ -1,13 +1,18 @@
 package com.danira0037.assesment3.ui.screen
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.danira0037.assesment3.model.Buku
 import com.danira0037.assesment3.network.BukuApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
+
+    var data = mutableStateOf(emptyList<Buku>())
+        private set
 
     init {
         retrieveData()
@@ -16,8 +21,7 @@ class MainViewModel: ViewModel() {
     private fun retrieveData() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = BukuApi.service.getBuku()
-                Log.d("MainViewModel", "Success: $result")
+                data.value = BukuApi.service.getBuku()
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
